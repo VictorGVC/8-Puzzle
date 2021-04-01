a913ca3ab86c8e07fa0d14d64e75d89fca22c305
HITRON <vicgabriel17@hotmail.com>
2021-04-01 16:03:13 -0300

    - Objetos com ID

--- EightPuzzle/src/eightpuzzle/TelaPrincipalController.java
+++ EightPuzzle/src/eightpuzzle/TelaPrincipalController.java
@@ -12,12 +12,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 /**
  *
  * @author vicga
  */
 public class TelaPrincipalController implements Initializable {
+
+    @FXML
+    private GridPane gpimage;
+    @FXML
+    private ImageView im00;
+    @FXML
+    private ImageView im01;
+    @FXML
+    private ImageView im02;
+    @FXML
+    private ImageView im10;
+    @FXML
+    private ImageView im11;
+    @FXML
+    private ImageView im12;
+    @FXML
+    private ImageView im20;
+    @FXML
+    private ImageView im21;
+    @FXML
+    private ImageView im22;
     
     
     
