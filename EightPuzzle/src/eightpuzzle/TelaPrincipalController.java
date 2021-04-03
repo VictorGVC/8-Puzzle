718ef028f6a2047e1985377c3548cf41b1e31384
HITRON <vicgabriel17@hotmail.com>
2021-04-03 10:18:29 -0300

    - Imagem agora está devidamente espaçada

--- EightPuzzle/src/eightpuzzle/TelaPrincipalController.java
+++ EightPuzzle/src/eightpuzzle/TelaPrincipalController.java
@@ -15,6 +15,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
